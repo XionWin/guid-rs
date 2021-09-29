1addaaf79f5032c1d36625497de90c37b0a71105
Xion <xionwin@hotmail.com>
2021-09-30 01:17:45 +0800

    Update

--- guid/app/src/main.rs
+++ guid/app/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     drawing::begin_render!(render, render_frame, &mut es_context);
 }
 
-fn render(graphic: &drawing::Graphic) -> (u32, std::time::SystemTime) {
+fn render(graphic: &drawing::Graphic) -> (Model, u32, std::time::SystemTime) {
     let model = Model::new();
 
     let mut vbos = vec![0u32; 2];
@@ -61,15 +61,16 @@ fn render(graphic: &drawing::Graphic) -> (u32, std::time::SystemTime) {
     );
 
     (
+        model,
         gles::get_uniform_location(program.get_id(), "model_mat"),
         std::time::SystemTime::now()
     )
 }
 
-fn render_frame(_graphic: &drawing::Graphic,  params: &mut (u32, std::time::SystemTime)) {
-    let (model_mat_location, first_tick) = params;
+fn render_frame(_graphic: &drawing::Graphic,  params: &mut (Model, u32, std::time::SystemTime)) {
+    let (model, model_mat_location, first_tick) = params;
     let angle = (std::time::SystemTime::now().duration_since(*first_tick).unwrap().as_millis() / 20 % 360) as u32;
-    let hsv = drawing::color::HSV::new(angle as f32, 1.0f32, 0.5f32);
+    let hsv = drawing::color::HSV::new(angle as f32, 1.0f32, 0.2f32);
     let rgb: drawing::color::RGB = hsv.into();
     let (r, g, b) = rgb.into();
 
@@ -77,7 +78,9 @@ fn render_frame(_graphic: &drawing::Graphic,  params: &mut (u32, std::time::Syst
     gles::clear(0x00004000);
     gles::bind_vertex_array(0);
     set_rotation_matrix(angle as f32 / 360f32 * std::f32::consts::PI * 2f32, *model_mat_location);
-    gles::draw_elements(gles::def::BeginMode::Triangles, 6, gles::def::DrawElementsType::UnsignedShort, std::ptr::null());
+    // gles::draw_elements(gles::def::BeginMode::Triangles, 3, gles::def::DrawElementsType::UnsignedShort, std::ptr::null());
+    gles::draw_arrays(gles::def::BeginMode::Points, 0, model.get_len() as _);
+    gles::draw_arrays(gles::def::BeginMode::Polygon, 0, model.get_len() as _);
 }
 
 fn set_rotation_matrix(rad: f32, model_mat_location: u32)
